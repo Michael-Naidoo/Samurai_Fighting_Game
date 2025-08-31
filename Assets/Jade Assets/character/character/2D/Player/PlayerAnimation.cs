2aad3a0c1474cc989a9d739fbb80a53b884d104a
JadeyyT <jadetiareddy@gmail.com>
2025-08-31 14:16:59 +0200

    Full Character with linked animations
    Walk
    LightAttack
    Block
    Jump - Press space, does animation then gets stuck (however, after it gets stuck if you press the left mouse button, if somehow works)
    Still need to figure out Sprinting
    Some animations need some slight tweaking

---
+++ Assets/Jade Assets/character/character/2D/Player/PlayerAnimation.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    public Animator animator;
+
+    public float runSpeed = 40f;
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool isWalking = false;
+    
+   
+
+    public UnityEvent OnLandEvent;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        animator.SetFloat("walkSpeed", Mathf.Abs(horizontalMove));
+        isWalking = true;
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            jump = true;
+            animator.SetBool("Jump", true);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            animator.SetBool("LightAttack", true);
+        }
+        else
+        {
+            animator.SetBool("LightAttack", false);
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            animator.SetBool("Blocking", true);
+        }
+        else
+        {
+            animator.SetBool("Blocking", false);
+        }
+
+        //if (isWalking && Input.GetKeyDown(KeyCode.LeftShift))
+        //{
+        //    animator.SetBool("Running", true);
+        //}
+        //else
+        //{
+        //   animator.SetBool("Running", false);
+        //}
+
+        //if (Input.GetKeyDown(KeyCode.LeftShift))
+        //{
+        // animator.SetFloat("runSpeed", Mathf.Abs(horizontalMove));
+        //}
+
+    }
+
+    //public void OnLanding()
+    //{
+    //    animator.SetBool("Jump", false);
+    //}
+    //public void AttackEnd()
+    //{
+    //    animator.SetBool("LightAttack", false);
+    //}
+}
